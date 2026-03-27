6cd59787be2b19d309ba92d41094b46baeef2507
Yuri Machado de Medeiros <yurimacha15@gmail.com>
2026-03-27 20:18:12 -0300

    Learning Loop For in C
    Here I made a program that print a multiplication table from the number that a user enter.

---
+++ codigosC/loop_for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, n;
+
+    printf("Type a number to see its multiplication table: \n");
+    scanf("%d", &n);
+
+    for (i == 0; i <= 10; i++) {
+
+        printf("%d * %d = %d\n", n, i, n * i);
+
+    }
+
+    printf("This is the %d multiplication table.\n", n);
+}
