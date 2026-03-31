c6add4237d978117e95a5ad5a74083331537e246
Yuri Machado de Medeiros <yurimacha15@gmail.com>
2026-03-31 20:04:21 -0300

    Update loop_for.c
    The update is the documentation to the code.

--- codigosC/loop_for.c
+++ codigosC/loop_for.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int main() {
+    // This program prints the multiplication table of a given number using a for loop.
+
+    // Declare variables for the loop counter and the number for which the multiplication table will be generated.
+
     int i, n;
 
+    // Prompt the user to enter a number and read the input.
     printf("Type a number to see its multiplication table: ");
     scanf("%d", &n);
 
+    // Use a for loop to iterate from 0 to 10 and print the multiplication table of the given number.
     for (i = 0; i <= 10; i++) {
 
         printf("%d * %d = %d\n", n, i, n * i);
