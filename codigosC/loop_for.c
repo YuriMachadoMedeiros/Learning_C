38bdf1fe843ec52197aeff6b76f24083d77dfc08
Yuri Machado de Medeiros <yurimacha15@gmail.com>
2026-03-31 19:54:35 -0300

    Update loop_for.c

--- codigosC/loop_for.c
+++ codigosC/loop_for.c
@@ -3,14 +3,14 @@
 int main() {
     int i, n;
 
-    printf("Type a number to see its multiplication table: \n");
+    printf("Type a number to see its multiplication table: ");
     scanf("%d", &n);
 
-    for (i == 0; i <= 10; i++) {
+    for (i = 0; i <= 10; i++) {
 
         printf("%d * %d = %d\n", n, i, n * i);
-
     }
 
-    printf("This is the %d multiplication table.\n", n);
+    printf("Multiplication table of %d completed.\n", n);
+    return 0;
 }
